fd6c1620204001f04c5bfafaea5cd808ad64b5fb
juja256 <melkor_g@list.ru>
2013-02-18 16:50:52 +0200

    initial commit

--- AddException.h
+++ AddException.h
@@ -1,13 +1,12 @@
 #ifndef ADDEXCEPTION_H
 #define ADDEXCEPTION_H
 #include <exception>
-// клас-виняток - невалідне додавання
 
 class AddException:public std::exception
 {
 	virtual const char* what() const throw()
 	{
-		return "Невірне додавання";
+		return "not valid composition";
 	}
 };
 
