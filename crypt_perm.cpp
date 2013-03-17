bcae5bedefc0534ccc3e78673be2769c0c618424
juja256 <melkor_g@list.ru>
2013-03-17 21:14:40 +0200

    succesfully compiled. not yet tested

--- crypt_perm.cpp
+++ crypt_perm.cpp
@@ -7,49 +7,49 @@
 #include "crypt_perm.h"
 #include <iostream>
 
-void align(char* data, int block_len)
+void align(char* data, u_int block_len)
 {
-	int numBytes = strlen(data);
+	u_int numBytes = strlen(data);
 	if (numBytes%block_len!=0) 
 	{
 		char* tmp = new char[numBytes + numBytes%block_len+1];
 		strcpy(tmp, data);
-		for (int i=numBytes; i<numBytes + numBytes%block_len; i++) tmp[i]=' ';
+		for (u_int i=numBytes; i<numBytes + numBytes%block_len; i++) tmp[i]=' ';
 		tmp[numBytes + numBytes%block_len]='\0';
 		strcpy(data, tmp);
 	}
 }
 
-template <int block_len>
-std::vector< std::bitset<8> > explodeBitset( std::bitset<block_len> block )
+/*std::vector< std::bitset<8> > explodeBitset( Block& block )
 {
+	u_int block_len = block.size;
 	std::vector< std::bitset<8> > res( block_len/8 );
 	for (int i=0; i<block_len; i++)
 		res[i/8][i%8]=block[i];	
 	return res;
-}
+}*/
 
-template <int block_len>
-std::vector< std::bitset<block_len> > bytesToBitsetArray(char *data)
+std::vector< Block > bytesToBitsetArray(char *data, u_int block_len)
 {
-	int numBytes = strlen(data);
-	int numKeyBytes = block_len / 8;
-	int numBlocks;
+	u_int numBytes = strlen(data);
+	u_int numKeyBytes = block_len / 8;
+	u_int numBlocks;
 	if (numBytes%numKeyBytes==0)
 		numBlocks = numBytes * 8 / block_len;
 	else
-		numBlocks = numBytes * 8 / block_len + 1;
+		numBlocks = numBytes / numKeyBytes + numBytes%numKeyBytes;
 	align(data, numKeyBytes);
 
-    std::vector< std::bitset<block_len> > b(numBlocks);
+    std::vector< Block > b(numBlocks);
 
-    for(int i = 0; i < numBlocks; ++i)
+    for(u_int i = 0; i < numBytes; i+=numKeyBytes)
     {
+    	b[i]=Block(block_len);
         char block[numKeyBytes];
         char cur;
-        for (int j = 0; j < numKeyBytes; ++j) 
+        for (u_int j = 0; j < numKeyBytes; ++j) 
         	block[j] = data[i+j];
-        for(int bit = 0; bit < block_len; ++bit)
+        for(u_int bit = 0; bit < block_len; ++bit)
         {
         	cur = block[bit/8];
         	b[i][bit] = cur & 1;
@@ -62,27 +62,24 @@ std::vector< std::bitset<block_len> > bytesToBitsetArray(char *data)
 
 const char* encode(const Permutation& perm, char* text) 
 {
-	int block_len = perm.k;
+	u_int block_len = perm.k;
 	if (block_len & (block_len-1)) return "invalid permutation, number of elements must be power of 2";
-	int numKeyBytes = block_len /8;
-	int numBytes = strlen(text);
-	int alignedBytes = numBytes + numBytes%keyBytes;
-	int numBlocks = alignedBytes / numKeyBytes;
+	u_int numKeyBytes = block_len /8;
+	u_int numBytes = strlen(text);
+	u_int alignedBytes = numBytes + numBytes%numKeyBytes;
 	char* out=new char[alignedBytes + 1];
 	out[alignedBytes]='\0';
-	std::vector< std::bitset<block_len> > bytes = bytesToBitsetArray<block_len>(text);
-	for (int i=0; i<numBytes; i+=numKeyBytes)
+	std::vector< Block > bytes = bytesToBitsetArray(text, block_len);
+	for (u_int i=0; i<numBytes; i+=numKeyBytes)
 	{
-		std::bitset<block_len> block;
+		Block block(block_len);
 		/* permutate bits in block */
-		for (int bit=0; bit<block_len; ++bit) 
+		for (u_int bit=0; bit<block_len; ++bit) 
 			block[bit]=bytes[i][perm[bit]-1];
-		/* explode bitset<block_len> into bitset<8> */
-		std::vector< std::bitset<8> > b = explodeBitset<block_len>(block);
 		
 		/* write encoded block into output */
-		for (int j=0; j<numKeyBytes; ++j) 
-			out[i+j]=char(b[j].to_ulong());
+		for (u_int j=0; j<numKeyBytes; ++j) 
+			out[i+j]=char(block.bytes[j].to_ulong());
 	}
 	return out;
 }
--- crypt_perm.h
+++ crypt_perm.h
@@ -12,26 +12,33 @@
 #include <cstring>
 #include "Permutation.h"
 
+typedef unsigned int u_int;
+
+struct Block 
+{
+	std::bitset<8>* bytes;
+	const u_int size;
+	Block(int n) : size(n) { bytes = new std::bitset<8>[n]; }
+	Block() : size(0) { }
+	~Block() { delete[] bytes; }
+	std::bitset<8>::reference operator[](u_int i) { return bytes[i/8][i%8]; }
+	Block& operator=(const Block& block) { return *this; }
+};
+
 /* 
  * Two functions that implement permutation encryption/decryption methods.
  * They take permutation(key of the cipher) and byte array that is needed to be encoded or decoded
  */
-
 
 const char* encode(const Permutation&, char*);
 
 const char* decode(const Permutation&, char*);
 
 /* Function that aligns byte array depended on number of blocks */ 
-void align(char*, int);
-
-/* Function that explodes 'large' bitset into smaller bitsets of 8 bits */
-template <int block_len>
-std::vector< std::bitset<8> > explodeBitset(std::bitset<block_len>);
+void align(char*, u_int);
 
 /* Function that explodes byte array into blocks of bits */
-template <int block_len>
-std::vector< std::bitset<block_len> > bytesToBitsetArray(char*);
+std::vector< Block > bytesToBitsetArray(char*, u_int);
 
 /* note: to convert const char* to char* use strdup() */
 #endif /* CRYPT_PERM_H */
